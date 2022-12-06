66b5a3775d4891caebe8f61bb49aff9a986cd189
shalini bhatt <shalinibhatt987@gmail.com>
2022-12-06 21:01:14 +0530

    prime_and_pallindrome

---
+++ Checking_Palindrome_no.js
@@ -0,0 +1,13 @@
+let str="Naman"
+let str1=""
+
+for(let i=str.length-1; i>=0; i--){
+str1=str1+str[i]
+}
+if(str==str1){
+console.log("Yes")
+}
+else{
+console.log("No")
+}
+}
---
+++ Checking_Prime_no.js
@@ -0,0 +1,13 @@
+let c=0;
+let num=11;
+for(let i=0;i<=Num;i++){
+if(num%i==0){
+c++
+}
+}
+if(c=2){
+console.log("It is a Prime No.)
+}
+else{
+console.log(It is not a Prime No.)
+}
